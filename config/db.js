949bade92444f4578a1f88291f37a687ce08a739
davirsreis <daviresende.2312@gmail.com>
2022-10-12 01:02:30 -0300

    upload

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://davirsreis:Jd89pXJ&+styxj@blogapp-davirsreis.khhqhfx.mongodb.net/?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://davirsreis:Po5dgJMO9YHVxyPV@blogapp-davirsreis.khhqhfx.mongodb.net/?retryWrites=true&w=majority"}
 }else {
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
