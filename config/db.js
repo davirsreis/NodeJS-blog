708a9a81fb8c3e92dca06784426ee04238294ab0
Davi Resende <94652177+davirsreis@users.noreply.github.com>
2022-10-11 23:26:04 -0300

    Delete db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://davirsreis:Jd89pXJ&+styxj@blogapp-davirsreis.khhqhfx.mongodb.net/?retryWrites=true&w=majority'}
-}else {
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
-}
